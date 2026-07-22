7f50a0654f471cc45127fd0a097b1ceb26ee5db0
BENYAHIA Boualem <b_benyahia@outlook.com>
2026-07-22 06:33:49 +0200

    update

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -24,7 +24,7 @@ export const profile = {
   location: 'Medea, Algeria',
   email: 'benyahia.boualem@yahoo.com',
   phone: '+213 673 128 102',
-  cvUrl: '/cv.pdf',
+  cvUrl: '/CV-BoualemBENYAHIA.pdf',
   avatarText: 'BB',
   avatarImage: '/public/MyPic.jpeg',
   social: {
